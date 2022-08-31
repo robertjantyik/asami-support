6f836a9f4d95ff9d4bee4bc7326d795e443a8ae1
robert.jantyik <robert.jantyik@dextraline.hu>
2022-08-31 16:18:27 +0200

    component scan

--- src/main/java/hu/asami/asamisupport/AsamiSupportApplication.java
+++ src/main/java/hu/asami/asamisupport/AsamiSupportApplication.java
@@ -2,8 +2,10 @@ package hu.asami.asamisupport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "hu.asami" })
 public class AsamiSupportApplication {
 
     public static void main(String[] args) {
