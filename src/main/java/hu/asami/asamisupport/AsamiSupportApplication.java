1d5fae3edca3eed5e8361c882e25fa7f8a1e61d7
robert.jantyik <robert.jantyik@dextraline.hu>
2022-08-31 12:31:42 +0200

    init

---
+++ src/main/java/hu/asami/asamisupport/AsamiSupportApplication.java
@@ -0,0 +1,13 @@
+package hu.asami.asamisupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsamiSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsamiSupportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/asami/asamisupport/AsamiSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.asami.asamisupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsamiSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
